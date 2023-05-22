a91658c201ccf9f1cb560f1298f54440af081142
Norbu Dorji <86556182+norbu123@users.noreply.github.com>
2023-05-22 09:41:52 +0600

    Update urls.py

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
-from myapp import views 
+from myapp import views
 from django.conf import settings
 
 urlpatterns = [
-    path('',views.home, name='home'),
-    path('back/',views.back_view,name='back'),
+    path('', views.home, name='home'),
+    path('back/', views.back_view, name='back'),
     path('form', views.classify_image, name='classify_image'),
     path('classify', views.classify_captured_image, name='classify'),
-    
-    if settings.DEBUG:
+]
+
+if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-]
