c1f9e911cd25fab99fd662bb05b26164329d37eb
Norbu Dorji <86556182+norbu123@users.noreply.github.com>
2023-05-22 22:51:22 +0600

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -88,45 +88,47 @@ def classify_captured_image(request):
     # Load the model from the file
     model_path = os.path.join(settings.BASE_DIR, 'models', 'currencyDetector.h5')
     model = load_model(model_path)
-    
+
     # Start the video capture
     cap = cv2.VideoCapture(0)
-    
+
+    label = None  # Initialize label
+
     while True:
         # Read a frame from the video capture
         ret, frame = cap.read()
-        
+
         # Check if a valid frame was obtained
         if not ret:
             break
-        
+
         # Resize the frame
         frame_resized = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
-        
+
         # Convert the frame to a numpy array
         frame_array = np.array(frame_resized)
-        
+
         # Rescale the pixel values to the range [0, 1]
         frame_array = frame_array.astype('float32') / 255.0
-        
+
         # Add a batch dimension to the array
         frame_array = np.expand_dims(frame_array, axis=0)
-        
+
         # Make a prediction using the model
         prediction = model.predict(frame_array)
         label = decode_prediction(prediction)
-        
+
         # Display the classification result on the video capture window
         cv2.putText(frame, label, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow('Identification Result', frame)
-        
+
         # Wait for the user to press a key
         key = cv2.waitKey(1)
-        
+
         # If the user presses the 'q' key, exit the loop
         if key == ord('q'):
             break
-    
+
     # Release the video capture and destroy the window
     cap.release()
     cv2.destroyAllWindows()
