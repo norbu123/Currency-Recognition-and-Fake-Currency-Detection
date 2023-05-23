a43954a1ce2572483ba03c926f3c89db58cb2031
Norbu Dorji <86556182+norbu123@users.noreply.github.com>
2023-05-24 00:09:38 +0600

    Update urls.py

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('back/', views.back_view, name='back'),
-    path('form/', views.classify_image, name='classify_image'),
+    path('form', views.classify_image, name='classify_image'),
     path('classify/', views.classify_captured_image, name='classify'),
 ]
 
